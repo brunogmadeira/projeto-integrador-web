fc5d03df2424b687b9d11d3753c56e31135f5128
rafafrassetto <rafafrass@gmail.com>
2024-10-04 01:19:15 -0300

    chore: adição da logo no menu

--- front-end/src/components/header/header-navigation.tsx
+++ front-end/src/components/header/header-navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link'; 
+import Image from 'next/image';
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -31,16 +32,31 @@ export function HeaderNavigation() {
     },
   ];
 
+  const fontStyle = {
+    fontFamily: 'Poppins, sans-serif',
+    fontWeight: '600',
+  };
+
   return (
-    <NavigationMenu className="flex justify-center items-center h-16 p-4 flex justify-center">
-      <div className="ml-80 p-4"> 
+    <NavigationMenu className="flex items-center h-24 bg-[#6C44A4] w-full -mx-29">
+      <div className="flex items-center justify-between w-full px-20 pr-80">
+        <Image 
+          src="/assets/images/header/logo-login.png"
+          alt="Logo"
+          width={130}
+          height={130} 
+          layout="intrinsic"
+          className="mr-4"
+          style={{ marginBottom: '0rem' }}
+        />
         <NavigationMenuList className="flex space-x-8"> 
           {HEADER_NAVIGATION.map((item) => (
             <NavigationMenuItem key={item.title}>
               <Link
                 id={item.title + '-element'}
                 href={item.href}
-                className={`${navigationMenuTriggerStyle()} text-black text-xl font-bold transition-colors bg-transparent hover:bg-white hover:text-black`}
+                className={`${navigationMenuTriggerStyle()} text-black text-2xl transition-colors bg-transparent hover:bg-white hover:text-black`}
+                style={fontStyle}
               >
                 {item.title}
               </Link>
