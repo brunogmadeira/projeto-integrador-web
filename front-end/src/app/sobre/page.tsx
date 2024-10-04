6bcb81622cec21cad9deac2f1976d9ecfe842545
rafafrassetto <rafafrass@gmail.com>
2024-10-04 03:17:53 -0300

    feat : adição do título na tela sobre

---
+++ front-end/src/app/sobre/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import { HeaderNavigation } from '@/components/header/header-navigation';
+import Title from '@/app/sobre/components/title';
+
+const Sobre = () => {
+    return (
+      <div>
+        <HeaderNavigation />
+        <Title />
+      </div>
+    );
+  };
+  
+export default Sobre;
