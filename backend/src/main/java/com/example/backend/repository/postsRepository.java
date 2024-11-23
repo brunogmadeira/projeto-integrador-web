32503d8377577f02aa927ba1f0aa22fa1b3e475a
Bruno Girardi Madeira <girardi791@gmail.com>
2024-11-22 22:14:11 -0300

    Feature: adicionar busca por filtros de animais na barra de pesquisa

--- backend/src/main/java/com/example/backend/repository/postsRepository.java
+++ backend/src/main/java/com/example/backend/repository/postsRepository.java
@@ -14,7 +14,9 @@ public interface postsRepository extends JpaRepository<postCad, Integer> {
     @Query("SELECT p FROM postCad p")
     List<postCad> findAll();
 
-    @Query("SELECT p FROM postCad p WHERE p.status = 1 AND p.titulo LIKE %:titulo% or p.descricao LIKE %:titulo%")
+    @Query("SELECT p FROM postCad p WHERE p.status = 1 AND p.titulo LIKE %:titulo% OR p.descricao LIKE %:titulo% OR " +
+            "p.filtro_animal LIKE %:titulo% OR p.filtro_raca LIKE %:titulo% OR p.filtro_porte LIKE %:titulo% OR " +
+            "p.filtro_causa LIKE %:titulo%")
     List<postCad> findAllByFilter(@Param("titulo") String titulo);
 
 }
